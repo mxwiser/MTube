48a7ec3b3abe6fec6d77dfba375a4c3f605b2184
vsine <proulx@yeah.net>
2020-05-19 03:15:22 +0800

    update

--- app/src/main/java/com/qvqol/mtube/ui/FirstItem/FirstFragment.java
+++ app/src/main/java/com/qvqol/mtube/ui/FirstItem/FirstFragment.java
@@ -2,9 +2,11 @@ package com.qvqol.mtube.ui.FirstItem;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +15,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -24,8 +28,11 @@ public class FirstFragment extends Fragment {
     public FirstFragment(){
 
     }
+    private MyAdapter myAdapter;
     private   SwipeRefreshLayout srl;
     private RecyclerView recyclerView;
+    private Handler handler;
+    private LinearLayoutManager linearLayoutManager;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -39,7 +46,8 @@ public class FirstFragment extends Fragment {
 
 
     private void initView(){
-
+        handler=new Handler();
+        linearLayoutManager=new LinearLayoutManager(getContext());
         srl.setColorSchemeResources(R.color.feng);
         srl.setRefreshing(true);
         srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -49,7 +57,11 @@ public class FirstFragment extends Fragment {
             }
         });
         reView();
+        myAdapter=new MyAdapter(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
+        recyclerView.setAdapter(myAdapter);
     }
 
 
@@ -58,5 +70,39 @@ public class FirstFragment extends Fragment {
     public  void  reView(){
         Toast.makeText(getContext(),"hello",Toast.LENGTH_LONG).show();
         srl.setRefreshing(false);
+    }
+
+    private void getData(final String type) {
+        if ("reset".equals(type)) {
+            listData.clear();
+            count = 0;
+            for (int i = 0; i < 3; i++) {
+                count += 1;
+                listData.add(count);
+            }
+        }
+        else if ("refresh".equals(type)) {
+            listData.clear();
+            count = 0;
+            for (int i = 0; i < 13; i++) {
+                count += 1;
+                listData.add(count);
+            }
+        } else {
+            for (int i = 0; i < 3; i++) {
+                count += 1;
+                listData.add(count);
+            }
+        }
+
+        myAdapter.notifyDataSetChanged();
+        if (srl.isRefreshing()) {
+            srl.setRefreshing(false);
+        }
+        if ("refresh".equals(type)) {
+            Toast.makeText(getContext(), "刷新完毕", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "加载完毕", Toast.LENGTH_SHORT).show();
+        }
     }
 }
