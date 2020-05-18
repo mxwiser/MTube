f0c58d69199aadf9027f6068d1c8201a5cf037aa
vsine <proulx@yeah.net>
2020-05-19 01:58:50 +0800

    update

--- app/src/main/java/com/qvqol/mtube/ui/FirstItem/MyAdapter.java
+++ app/src/main/java/com/qvqol/mtube/ui/FirstItem/MyAdapter.java
@@ -5,7 +5,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final static int TYPE_CONTENT=0;//正常内容
+    private final static int TYPE_FOOTER=1;//下拉刷新
+    private ArrayList<String> list;
+
+
+    public  MyAdapter(){
+
+        list=new ArrayList<>();
+        list.add("a");
+        list.add("b");
+    }
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -20,5 +33,10 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
     }
 }
