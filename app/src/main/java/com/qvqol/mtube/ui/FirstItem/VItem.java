9f97bc13951559ffdc48f56eb25d8bed89e715d4
vsine <proulx@yeah.net>
2020-05-19 07:31:39 +0800

    update

---
+++ app/src/main/java/com/qvqol/mtube/ui/FirstItem/VItem.java
@@ -0,0 +1,15 @@
+package com.qvqol.mtube.ui.FirstItem;
+
+import android.service.chooser.ChooserTarget;
+import android.widget.ImageView;
+
+public class VItem {
+    //0 video 1 ad
+    public int category=0;
+
+    public String title=" ";
+    public String time="";
+    public String hot="";
+    public String duration="";
+    public ImageView imageView;
+}
