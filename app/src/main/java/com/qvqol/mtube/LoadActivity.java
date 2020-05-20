9902661e2101e7fd8dc13fa6824b66a49b99247b
vsine <proulx@yeah.net>
2020-05-20 08:43:39 +0800

    update

--- app/src/main/java/com/qvqol/mtube/LoadActivity.java
+++ app/src/main/java/com/qvqol/mtube/LoadActivity.java
@@ -3,7 +3,6 @@ package com.qvqol.mtube;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,35 +10,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.qvqol.mtube.ui.FirstItem.ImageTask;
 
 public class LoadActivity extends AppCompatActivity {
+    Handler handler;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getSupportActionBar().hide();
         setContentView(R.layout.load);
-        final Handler handler=new Handler();
+      handler=new Handler();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i=0;i<5;i++){
-                    ImageTask.loadImageFromNetwork("https://qvqol.com/img.png","ItemImage"+i);
-
-                }
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent i=new Intent(LoadActivity.this,MainActivity.class);
-                        startActivity(i);
-                        finish();
-                    }
-                },500);
+               loadMainItemImage();
+               startMain();
             }
         }).start();
+    }
 
-
-
-
-
+    private  void loadMainItemImage(){
+        for (int i=0;i<5;i++){
+            ImageTask.loadImageFromNetwork("https://qvqol.com/img.png","ItemImage"+i);
+        }
+    }
+    private void  startMain(){
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(LoadActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },500);
     }
 }
