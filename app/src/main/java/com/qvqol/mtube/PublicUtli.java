0a1cd391fbed5202f3ad3d8285c4f60021b89a9b
vsine <proulx@yeah.net>
2020-05-20 12:39:03 +0800

    update

--- app/src/main/java/com/qvqol/mtube/PublicUtli.java
+++ app/src/main/java/com/qvqol/mtube/PublicUtli.java
@@ -6,7 +6,7 @@ import com.bumptech.glide.Glide;
 
 public class PublicUtli {
 
-    //清楚Glide的内存缓存和磁盘缓存
+    //清除Glide的内存缓存和磁盘缓存
     public static void clearGlideCache(Context context){
         Glide.get(context).clearDiskCache();
         Glide.get(context).clearMemory();
