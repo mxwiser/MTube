6fccad906e61e3a6671c79f694e5b36490fdd89c
vsine <proulx@yeah.net>
2020-06-08 15:05:08 +0800

    update

--- app/src/main/java/com/qvqol/mtube/ui/FirstItem/MyAdapter.java
+++ app/src/main/java/com/qvqol/mtube/ui/FirstItem/MyAdapter.java
@@ -79,14 +79,14 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         listItem.add(vItem);
     };
 
-    public void addDateAd(String title, String message, String img,String adurl,boolean isOpen){
+    public void addDateAd(String title, String message, String img,String abdul,boolean isOpen){
         VItem vItem=new VItem();
         vItem.title=title;
         vItem.message=message;
         vItem.category=1;
         vItem.imgUrl=img;
         vItem.isOpenOnApp=isOpen;
-        vItem.adurl=adurl;
+        vItem.adurl=abdul;
         listItem.add(vItem);
     };
 
