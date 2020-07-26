096fa66d6aaeb32ef26629a3074d40d5c877a8b3
vsine <proulx@yeah.net>
2020-07-26 17:14:16 +0800

    update

--- app/src/main/java/com/qvqol/mtube/ui/FirstItem/MyAdapter.java
+++ app/src/main/java/com/qvqol/mtube/ui/FirstItem/MyAdapter.java
@@ -1,11 +1,6 @@
 package com.qvqol.mtube.ui.FirstItem;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
