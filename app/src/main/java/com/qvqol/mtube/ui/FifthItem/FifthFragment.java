653f365c3ffeec9719cb75b9681ecf8da6da50e4
vsine <proulx@yeah.net>
2020-08-26 18:27:10 +0800

    update

--- app/src/main/java/com/qvqol/mtube/ui/FifthItem/FifthFragment.java
+++ app/src/main/java/com/qvqol/mtube/ui/FifthItem/FifthFragment.java
@@ -23,5 +23,6 @@ public class FifthFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_fifth, container, false);
         root.setBackgroundColor(Color.WHITE);
         return root;
+
     }
 }
