fa602a96427f7bff1eae53d47e2635c174ed683c
vsine <proulx@yeah.net>
2020-08-26 18:44:01 +0800

    update

--- app/src/main/java/com/qvqol/mtube/LoadActivity.java
+++ app/src/main/java/com/qvqol/mtube/LoadActivity.java
@@ -52,7 +52,6 @@ public class LoadActivity extends AppCompatActivity {
                 Intent i=new Intent(LoadActivity.this,MainActivity.class);
                 startActivity(i);
                 finish();
-
             }
         },500);
     }
